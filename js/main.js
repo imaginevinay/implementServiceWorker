12b8321e027c16399bb3e463815f400c09e63b55
imagineVinay <vinay.varshney28@gmail.com>
2020-01-18 14:34:00 +0530

    checking url requests

--- js/main.js
+++ js/main.js
@@ -355,7 +355,7 @@
  // make sure sw is supported
  if ('serviceWorker' in navigator) {
      window.addEventListener('load', () => {
-         navigator.serviceWorker.register('../sw_cached_pages.js').then((res) => {
+         navigator.serviceWorker.register('../implementServiceWorker/sw_cached_pages.js').then((res) => {
              console.log("service worker registered!")
          }).catch((err) => {
              console.log("service worker error =>", err)
